6178948d3604eb11eeb0d320d0dfc1383606e215
Tom Price <tomprice1989@gmail.com>
2016-03-15 21:50:33 +0000

    working solution

--- maze.py
+++ maze.py
@@ -1,8 +1,7 @@
-
 import collections
 from enum import Enum
+import time
 
-#VisitedCell = collections.namedtuple("VisitedCell", ["up", "down", "left", "right"])
 MazeCell = collections.namedtuple("MazeCell", ["x", "y"])
 
 class Direction(Enum):
@@ -10,6 +9,7 @@ class Direction(Enum):
 	down = 2
 	left = 3
 	right = 4
+	invalid = 5
 
 class Maze:
 	def open(self, file_name):
@@ -41,41 +41,44 @@ class Maze:
 	def move_token(self, old_cell, new_cell):
 		if not old_cell is None:
 			self.maze[old_cell.y][old_cell.x] = ' '
-			
-		self.maze[new_cell.y][new_cell.x] = 'O'
+		
+		if self.maze[new_cell.y][new_cell.x] != 'X':
+			self.maze[new_cell.y][new_cell.x] = 'O'
 		return new_cell
 
 	def get_cell_val(self, cell):
 		return self.maze[cell.y][cell.x]
 
-	def check_move_valid(self, cell, direction):
-		print(self.visited[cell.y][cell.x])
-		if self.get_cell_val(cell) != '#':
-			if direction == Direction.up:
-				if self.visited[cell.y][cell.x].up == False:
-					return True
-			elif direction == Direction.down:
-				if self.visited[cell.y][cell.x].down == False:
-					return True
-			elif direction == Direction.left:
-				if self.visited[cell.y][cell.x].left == False:
-					return True
-			elif direction == Direction.right:
-				if self.visited[cell.y][cell.x].right == False:
-					return True
+	def decrement_cell(self, cell):
+		self.visited[cell.y][cell.x] = self.visited[cell.y][cell.x] - 1
+			
+	def set_valid_moves(self, cell):		
+		incr = 0
+		best_dir = Direction.invalid
 
-		return False
-
-	def mark_cell_invalid(self, cell, direction):
-			t = self.visited[cell.y][cell.x]
-			if direction == Direction.up:				
-				self.visited[cell.y][cell.x] = VisitedCell(up=True, down=t.down, left=t.left, right=t.right)
-			elif direction == Direction.down:
-				self.visited[cell.y][cell.x] = VisitedCell(up=t.up, down=True, left=t.left, right=t.right)
-			elif direction == Direction.left:
-				self.visited[cell.y][cell.x] = VisitedCell(up=t.up, down=t.down, left=True, right=t.right)
-			elif direction == Direction.right:
-				self.visited[cell.y][cell.x] = VisitedCell(up=t.up, down=t.down, left=t.left, right=True)
+		if cell.x > 0 and self.maze[cell.y][cell.x-1] != '#':
+			incr += 1
+			if self.visited[cell.y][cell.x-1] == -1 or best_dir == Direction.invalid:
+				best_dir = Direction.left
+		if cell.x < self.width -1 and self.maze[cell.y][cell.x+1] != '#':
+			incr += 1
+			if self.visited[cell.y][cell.x+1] == -1 or best_dir == Direction.invalid:
+				best_dir = Direction.right
+		if cell.y > 0 and self.maze[cell.y-1][cell.x] != '#':
+			incr += 1
+			if self.visited[cell.y-1][cell.x] == -1 or best_dir == Direction.invalid:
+				best_dir = Direction.up
+		if cell.y < self.height -1 and self.maze[cell.y+1][cell.x] != '#':
+			incr += 1
+			if self.visited[cell.y+1][cell.x] == -1 or best_dir == Direction.invalid:
+				best_dir = Direction.down
+		
+		# only set the visited node count if we haven't been here before
+		if self.visited[cell.y][cell.x] == -1:
+			self.visited[cell.y][cell.x] = incr
+		
+		#print("Moves available = " + str(incr))
+		return best_dir
 
 	def print_maze(self):
 		for line in self.maze:
@@ -83,35 +86,46 @@ class Maze:
 			print(str)
 		print('\n')
 
+	def print_visited(self):
+		for line in self.visited:
+			for chr in line:			
+				print(str(chr), end=" ")
+			print('\n')
+		print('\n')
+
 	def solve(self):
 		current_cell = self.start_coords
 
 		test = 0;
 		while True:
-			if test == 5:
+			print("----------------------------------------------")
+			self.print_maze()
+
+			if self.maze[current_cell.y][current_cell.x] == 'X':
+				print("YOU WON")
+				break
+
+			best_dir = self.set_valid_moves(current_cell)
+			#self.print_visited()
+			
+			if best_dir == Direction.invalid:
+				print("Cannot continue, must be stuck in a loop or something")
 				break;
-
-			test += 1
-			# check up down left and right
-			#up
-			#print("Current cell x: " + str(current_cell.x) + " current cell y: " + str(current_cell.y))
-
-
-			if current_cell.y > 0 and self.check_move_valid(MazeCell(x=current_cell.x, y=current_cell.y -1), Direction.up):
-				self.mark_cell_invalid(current_cell, Direction.up)
+			elif best_dir == Direction.up:
+				self.decrement_cell(current_cell)
 				current_cell = self.move_token(current_cell, MazeCell(current_cell.x, current_cell.y - 1))
-				self.print_maze()
-				continue
-
-			if current_cell.y < self.height - 1 and self.check_move_valid(MazeCell(x=current_cell.x, y=current_cell.y + 1), Direction.down):
-				self.mark_cell_invalid(current_cell, Direction.down)
+			elif best_dir == Direction.down:
+				self.decrement_cell(current_cell)
 				current_cell = self.move_token(current_cell, MazeCell(current_cell.x, current_cell.y + 1))
-				self.print_maze()
-				continue
+			elif best_dir == Direction.left:
+				self.decrement_cell(current_cell)
+				current_cell = self.move_token(current_cell, MazeCell(current_cell.x -1, current_cell.y))
+			elif best_dir == Direction.right:
+				self.decrement_cell(current_cell)
+				current_cell = self.move_token(current_cell, MazeCell(current_cell.x + 1, current_cell.y))
 
-
+			time.sleep(0.5)
 
 maze = Maze()
 maze.open('maze.txt')
-maze.print_maze()
 maze.solve()
